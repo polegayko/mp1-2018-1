c627d47018f03c37d19fc9402e1d3ba6a9bfb34e
polegayko <victorpolegayko@gmail.com>
2018-03-16 17:27:17 +0300

    3.6

---
+++ students/polegayko_v/task3/Source.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Dictionary
+{
+private:
+	vector<string> EnglishWord;
+	vector<string> Translation;
+	int FindWord(string s)
+	{
+		for (int i = 0; i < EnglishWord.size(); i++)
+			if (EnglishWord[i] == s)
+				return i;
+		return -1;
+	}
+
+public:
+	Dictionary() {};
+	Dictionary(const Dictionary& ER)//ER - англо-русский
+	{
+		EnglishWord = ER.EnglishWord;
+		Translation = ER.Translation;
+	}
+	Dictionary& operator=(const Dictionary& d)
+	{
+		EnglishWord = d.EnglishWord;
+		Translation = d.Translation;
+		return *this;
+	}
+	int AddWord(string enword, string translation)
+	{
+		EnglishWord.push_back(enword);
+		Translation.push_back(translation);
+		return EnglishWord.size();
+	}
+	string& operator[](const string& e)
+	{
+		int i = FindWord(e);
+		if (i != -1)
+		return Translation[i];
+		else throw("error");
+	}
+	bool CheckWord(const string& enword)
+	{
+		if (FindWord(enword) != -1)
+			return true;
+		else return false;
+	}
+	int SizeD()
+	{
+		return EnglishWord.size();
+	}
+	void Save()
+	{
+		ofstream f("DictionaryER");
+		for (int i = 0; i < EnglishWord.size(); i++)
+		{
+			f << EnglishWord[i];
+			f << " - ";
+			f << Translation[i];
+			f << "\n";
+		}
+		f.close();
+	}
+	void Open()
+	{
+		EnglishWord.clear();
+		Translation.clear();
+		char b[200];
+		ifstream f("DictionaryER");
+		while (!f.eof())
+		{
+			f >> b;
+			string s1(b);
+			f >> b;
+			f >> b;
+			string s2(b);
+			AddWord(s1, s2);
+		}
+	}
+
+};
+void main()
+{
+	setlocale(LC_ALL, "");
+	Dictionary d;
+	d.AddWord("pot", "горшок");
+	d["pot"] = "горшок)";
+	cout << d["pot"];
+
+	system("pause");
+}
